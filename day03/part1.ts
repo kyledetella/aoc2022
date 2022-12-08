904f6800cfc5e69f76a316eebb2f9aaee5f36580
Kyle DeTella <kdetella@gmail.com>
2022-12-08 12:09:14 -0600

    feat: 🎄 Day 3, pt 1

---
+++ day03/part1.ts
@@ -0,0 +1,62 @@
+const alphabetLowercase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const alphabetUppercase = alphabetLowercase.map((letter) =>
+  letter.toUpperCase()
+);
+const input = await Deno.readTextFile("./day03/input.txt");
+
+const compartments = input.split("\n").map((compartment) => {
+  return [
+    compartment.substring(0, compartment.length / 2),
+    compartment.substring(compartment.length / 2),
+  ];
+});
+
+const duplicateItemsFromCompartments = compartments.map(
+  ([compartment1, compartment2]) => {
+    return compartment1.split("").filter((item) => {
+      return compartment2.includes(item);
+    });
+  }
+); // 8072
+
+console.log(
+  duplicateItemsFromCompartments
+    .map((items) => {
+      const [item] = items;
+      const lowercasePriorityValue = alphabetLowercase.indexOf(item);
+
+      if (lowercasePriorityValue > -1) {
+        return lowercasePriorityValue + 1;
+      }
+
+      return alphabetUppercase.indexOf(item) + 27;
+    })
+    .reduce((accum, val) => accum + val, 0)
+); //
