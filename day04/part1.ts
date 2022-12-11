daa07d640fd94d6b5513bfc480606995421ed24d
Kyle DeTella <kdetella@gmail.com>
2022-12-11 07:58:25 -0600

    feat: 🎄 Day 4, pt 1

---
+++ day04/part1.ts
@@ -0,0 +1,36 @@
+const input = await Deno.readTextFile("./day04/input.txt");
+
+// Convert ranges (["2-4"]) to iterables with each value represented ([2, 3, 4])
+const sectionAssignments = input.split("\n").map((pair) => {
+  return pair.split(",").map((sectionAssignment) => {
+    const [sectionStart, sectionEnd] = sectionAssignment.split("-").map(Number);
+
+    let i = sectionStart + 1;
+    const sections = [sectionStart];
+
+    while (i <= sectionEnd) {
+      sections.push(i);
+      i++;
+    }
+    return sections;
+  });
+});
+
+const overlappingAssignmentPairs = sectionAssignments.reduce(
+  (accum, [left, right]) => {
+    if (left.length <= right.length) {
+      if (left.every((num) => right.includes(num))) {
+        return accum + 1;
+      }
+    } else {
+      if (right.every((num) => left.includes(num))) {
+        return accum + 1;
+      }
+    }
+
+    return accum;
+  },
+  0
+);
+
+console.log(overlappingAssignmentPairs); // 487
