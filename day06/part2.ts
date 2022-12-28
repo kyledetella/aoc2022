b11e599aecc1d2a44de5debe9460d2b5e4569c68
Kyle DeTella <kdetella@gmail.com>
2022-12-28 07:47:36 -0600

    feat: 🎄 Day 6, pt 2

--- day06/part2.ts
+++ day06/part2.ts
@@ -1,17 +1,19 @@
 const input = await Deno.readTextFile("./day06/input.txt");
 
+const MESSAGE_LENGTH = 14;
 let i = 0;
-let markerStartIndex = 0;
+let messageStartIndex = 0;
 const hasDuplicateCharacters = (str: string) => /(.).*\1/.test(str);
 
 while (i < input.length) {
-  const group = input.slice(i, i + 4);
+  const group = input.slice(i, i + MESSAGE_LENGTH);
+
   if (hasDuplicateCharacters(group)) {
-    i += 4;
+    i++;
   } else {
-    markerStartIndex = i + 1;
+    messageStartIndex = i + MESSAGE_LENGTH;
     break;
   }
 }
 
-console.log(markerStartIndex);
+console.log(messageStartIndex);
